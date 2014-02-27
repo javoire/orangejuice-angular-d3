be550b14c27eb10f57959429a592c0a6286f21f7
jd <jd@spotify.com>
2014-02-27 20:34:44 +0100

    do not exclude json files from oj lol

--- source/_app/services/Users.js
+++ source/_app/services/Users.js
@@ -4,7 +4,7 @@ angular.module('app')
   .factory('Users', function($q, $http) {
     var d = $q.defer();
 
-    $http.get('_data/users.json').then(function(users) { // user signup per day
+    $http.get('/users.json').then(function(users) { // user signup per day
       var data;
 
       data = users.data.map(function(d) { // index 1 needs to be integer and not string.. or just change input data lol
